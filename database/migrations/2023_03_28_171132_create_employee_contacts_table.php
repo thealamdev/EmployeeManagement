3f9d6584d5832f4342c623cb3f11edb25d06d800
Shah Alam <shahalam141072@gmail.com>
2023-03-28 23:24:05 +0600

    contact table done

---
+++ database/migrations/2023_03_28_171132_create_employee_contacts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee_contacts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('phone1');
+            $table->string('phone2')->nullable();
+            $table->string('email2')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('zip')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee_contacts');
+    }
+};
